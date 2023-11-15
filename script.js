8a453ca7e533732ec9f57cfade44230d1cacffe1
DavideLai <davidelai21@yahoo.it>
2023-11-15 21:00:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+//ESERCIZI SUGLI IF: 
+
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+ESERCIZIO 2
+  Crea un blocco condizionale if/else per mostrare in console il messaggio corretto in ogni condizione.
+
+  num < 5 - mostra in console "Tiny"
+  num < 10 - mostra in console "Small"
+  num < 15 - mostra in console "Medium"
+  num < 20 - mostra in console "Large"
+  num >= 20 - mostra in console "Huge"
+*/
+
+/* let number; 
+
+if(number<5) {
+    console.log("Tiny");
+} else if (number<10) {
+    console.log("Small");
+} else if (number<15) {
+    console.log("Medium");
+} else if (number<20) {
+    console.log("Large");
+} else if (number>=20) {
+    console.log("Huge");
+}
+
+
+//ESERCIZI SUI CICLI: 
+
+/* ESERCIZIO 3
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso di "continue").
+*/
+
+for (let index = 0; index <= 10; index++) {
+    if(let===3){
+        continue;
+    }
+    if(let===8){
+        continue;
+    }
+    console.log(index);
+}
+console.log(index);
+
+/* ESERCIZIO 4
+  Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare the il valore corrente sia pari o dispari, e mostrare il risultato in console.
+*/
+
